46243138696429823a8feed1208241d4e4516338
Nickwasused <nickwasused.services@protonmail.com>
2022-08-01 18:08:27 +0200

    use aral api

--- api/[stationid].js
+++ api/[stationid].js
@@ -5,7 +5,7 @@ export default async function handler(request, response) {
     const { stationid } = request.query;
     try {
         const res = await fetch(
-            `https://tanken-api.nickwasused.com/api/get/aral?id=${stationid}`,
+            `https://api.tankstelle.aral.de/api/v2/stations/${stationid}/prices${stationid}`,
         )
         data = await res.json()
     } catch (e) {

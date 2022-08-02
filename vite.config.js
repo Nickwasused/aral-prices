6d3a72b576e92ee318b097864f61cf8d37938aa4
Nickwasused <nickwasused.services@protonmail.com>
2022-08-02 12:14:06 +0200

    add favicon and manifest

--- vite.config.js
+++ vite.config.js
@@ -17,13 +17,66 @@ export default defineConfig({
           ],
       },
       manifest: {
-          name: "streaming-list",
-          short_name: "streaming-list",
-          display: "standalone",
-          theme_color: "#343a40",
-          background_color: "#343a40",
-          icons: [],
-      }})
+        name: "Aral Preise",
+        short_name: "Aral",
+        display: "standalone",
+        theme_color: "#2f4f4f",
+        background_color: "#007db9",
+        icons: [
+          {
+            src: "favicon/android-chrome-36x36.png",
+            sizes: "36x36",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-48x48.png",
+            sizes: "48x48",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-72x72.png",
+            sizes: "72x72",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-96x96.png",
+            sizes: "96x96",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-144x144.png",
+            sizes: "144x144",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-256x256.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-384x384.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "favicon/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "any maskable",
+          },
+        ],
+      },
+    })
   ],
   resolve: {
     alias: {

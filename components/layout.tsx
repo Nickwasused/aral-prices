164a214f3f7e24ec3278d2992e2219486acdd1cf
Nickwasused <github.lix1s@simplelogin.co>
2022-08-26 16:37:58 +0200

    add favicon

--- components/layout.tsx
+++ components/layout.tsx
@@ -3,11 +3,22 @@ import { h, Fragment } from "preact";
 
 export const Head = () => (
     <head>
-        <meta name="description" content="Nick`s Tank Preis Liste" />
         <link rel="stylesheet" href="/base.css" />
         <link rel="stylesheet" href="/stationlist.css" />
         <link rel="stylesheet" href="/stationstats.css" />
-        <title>Nick`s Tank Preis Liste</title>
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="/favicon/site.webmanifest" />
+        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
+        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <meta name="msapplication-TileColor" content="#e0e0d1" />
+        <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png" />
+        <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
+        <meta name="theme-color" content="#e0e0d1" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <title>Nick`s Tank-Preis Liste</title>
     </head>
 );
 

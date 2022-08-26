625912e9de0537574b029a58835e85e5e582efbc
Nickwasused <github.lix1s@simplelogin.co>
2022-08-26 16:50:36 +0200

    better static links

--- components/layout.tsx
+++ components/layout.tsx
@@ -1,18 +1,19 @@
 /** @jsx h */
 import { h, Fragment } from "preact";
+import { asset } from "$fresh/runtime.ts";
 
 export const Head = () => (
     <head>
-        <link rel="stylesheet" href="/base.css" />
-        <link rel="stylesheet" href="/stationlist.css" />
-        <link rel="stylesheet" href="/stationstats.css" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/favicon/apple-touch-icon.png" />
-        <link rel="icon" type="image/png" sizes="32x32" href="/favicon/favicon-32x32.png" />
-        <link rel="icon" type="image/png" sizes="192x192" href="/favicon/android-chrome-192x192.png" />
-        <link rel="icon" type="image/png" sizes="16x16" href="/favicon/favicon-16x16.png" />
-        <link rel="manifest" href="/favicon/site.webmanifest" />
-        <link rel="mask-icon" href="/favicon/safari-pinned-tab.svg" color="#5bbad5" />
-        <link rel="shortcut icon" href="/favicon/favicon.ico" />
+        <link rel="stylesheet" href={asset('/base.css')} />
+        <link rel="stylesheet" href={asset('/stationlist.css')} />
+        <link rel="stylesheet" href={asset('/stationstats.css')} />
+        <link rel="apple-touch-icon" sizes="180x180" href={asset('/favicon/apple-touch-icon.png')} />
+        <link rel="icon" type="image/png" sizes="32x32" href={asset('/favicon/favicon-32x32.png')} />
+        <link rel="icon" type="image/png" sizes="192x192" href={asset('/favicon/android-chrome-192x192.png')} />
+        <link rel="icon" type="image/png" sizes="16x16" href={asset('/favicon/favicon-16x16.png')} />
+        <link rel="manifest" href={asset('/favicon/site.webmanifest')} />
+        <link rel="mask-icon" href={asset('/favicon/safari-pinned-tab.svg')} color="#5bbad5" />
+        <link rel="shortcut icon" href={asset('/favicon/favicon.ico')} />
         <meta name="msapplication-TileColor" content="#e0e0d1" />
         <meta name="msapplication-TileImage" content="/favicon/mstile-144x144.png" />
         <meta name="msapplication-config" content="/favicon/browserconfig.xml" />
